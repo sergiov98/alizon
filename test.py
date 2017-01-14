cda6f1b5c292708a5af58e36e8288d38b528f498
Sergio V <Sergio V>
2017-01-13 23:49:30 -0800

    Test this

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/result/<item>')
-def getMatchingItem(item):
-    #Daniil and Winson's code
-    matchedItem = "url" + " price"
-    return matchedItem
-
-
+@app.route("/")
+def hello():
+    return "Hello World!"
 
 if __name__ == "__main__":
     app.run(port=5000)
