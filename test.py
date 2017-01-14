36c6c941fd60c14045c4fc527cbcb77a887e862a
Sergio V <Sergio V>
2017-01-13 23:56:18 -0800

    Maybe

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    consumer=input("What is your name? ")
+    return "Hello " + consumer
 
 if __name__ == "__main__":
     app.run(port=5000)
